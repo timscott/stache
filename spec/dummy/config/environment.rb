75c88544e0b503247061a9f2b3dd587a8ba63929
Tim Scott <tscott@lunaversesoftware.com>
2019-08-12 15:16:07 -0500

    Eager load instead of auto load.

--- spec/dummy/config/environment.rb
+++ spec/dummy/config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-Dummy::Application.config.autoload_paths << Rails.root + 'app/views'
+Dummy::Application.config.eager_load_paths << Rails.root + 'app/views'
 
 # Initialize the rails application
 Dummy::Application.initialize!
